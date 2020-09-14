c7f90da0174ba9c7503245235a611a67a902d52d
Zygimantas Daunoras <daunoraszygimantas@gmail.com>
2020-09-14 17:20:31 +0000

    I made an Army tank!

--- index.java
+++ index.java
@@ -14,7 +14,15 @@ void draw() {
   //this rectangle is the start of your army tank
   //all of your programming should be after this 👍 
   rect(200, 200, 100, 50);
-  
+  rect(175, 160, 50, 25);
+  rect(250, 155, 100, 10);
+  ellipse(150, 225, 20, 20);
+  ellipse(170, 225, 20, 20);
+  ellipse(190, 225, 20, 20);
+  ellipse(210, 225, 20, 20);
+  ellipse(230, 225, 20, 20);
+  ellipse(250, 225, 20, 20);
+
   
  
 
